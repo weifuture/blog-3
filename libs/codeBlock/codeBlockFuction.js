63e679cffc845885d4e26cd8c9d02c0356892ec1
weifuture <weifuture@users.noreply.github.com>
2020-03-12 03:56:37 +0000

    10001 weifuture/blog-hexo@9b37e4c74cd502e6dacd9fb27ee194db8ee4409f

---
+++ libs/codeBlock/codeBlockFuction.js
@@ -0,0 +1,3 @@
+// build time:Thu Mar 12 2020 03:56:34 GMT+0000 (Coordinated Universal Time)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
---
+++ libs/codeBlock/codeLang.js
@@ -0,0 +1,3 @@
+// build time:Thu Mar 12 2020 03:56:34 GMT+0000 (Coordinated Universal Time)
+$(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
+//rebuild by neat 
